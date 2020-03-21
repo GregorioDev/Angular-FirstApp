16955d75a3808726a8e8c477f8e937d1c51d8168
Gregorio <Gregorio@DESKTOP-6VLKNAE>
2020-03-21 11:04:38 -0300

    primeiro

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('O produto foi compartilhado!');
+  }
+
+  onNotify() {
+    window.alert('Você será notificado quando o produto for colocado à venda');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'IPhone X',
+    price: 799,
+    description: 'Um telefone grande com uma das melhores telas'
+  },
+  {
+    name: 'Moto G5s',
+    price: 699,
+    description: 'Um ótimo celular com uma das melhores câmeras'
+  },
+  {
+    name: 'Samsung S10',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
